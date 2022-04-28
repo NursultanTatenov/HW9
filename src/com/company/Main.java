da17a7556a63e2434145e2ff45d902293e8c6597
Beksultan Tatenov <bekatatenov@gmail.com>
2022-04-28 21:39:40 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int []array=new int[10];
+        getArray(array);
+        getArray2(array);
+        getArrayJupSan(array);
+
+        }
+
+
+
+    static int[] getArray(int a[]) {
+        Random random = new Random();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = random.nextInt(100);
+            System.out.print(a[i] + " ");
+        }
+        return a;
+
+
+
+    }
+
+    static int [] getArrayTakSan(int b[]) {
+        System.out.print("\nТак сандар: ");
+        for (int i = 0; i < b.length; i++) {
+            if (b[i]%2==1) {
+                System.out.print(b[i]+",");
+            }
+        }
+        return b;
+    }
+
+    static int[] getArray2(int c[]) {
+        getArrayTakSan(c);
+        return c;
+    }
+    static int [] getArrayJupSan(int d[]) {
+        System.out.print("\nЖуп сан: ");
+        for (int i = 0; i < d.length; i++) {
+            if (d[i]%2==0) {
+                System.out.print(d[i]+",");
+            }
+        }
+        return d;
+    }
+    }
